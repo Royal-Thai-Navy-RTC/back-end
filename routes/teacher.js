1e44a98edf1c768fa5d35d035e1e614ddf30b53d
xNewz <pargornruasijan@gmail.com>
2025-12-10 07:56:36 +0700

    fixed: middleware

--- routes/teacher.js
+++ routes/teacher.js
@@ -24,7 +24,7 @@ router.get(
 router.get(
   "/teacher/training-reports/export",
   middleware.verifyToken,
-  middleware.authorizeTeacher,
+  middleware.authorizeAdmin,
   teacherReportController.exportTrainingReportsExcel
 );
 
